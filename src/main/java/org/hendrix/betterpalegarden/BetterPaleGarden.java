96b3631e45baeb5a5ac6387310e7c1ccbb505e93
JimiIT92 <jimiit92@gmail.com>
2024-12-01 12:47:04 +0100

    Initial commit

---
+++ src/main/java/org/hendrix/betterpalegarden/BetterPaleGarden.java
@@ -0,0 +1,10 @@
+package org.hendrix.betterpalegarden;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BetterPaleGarden implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
